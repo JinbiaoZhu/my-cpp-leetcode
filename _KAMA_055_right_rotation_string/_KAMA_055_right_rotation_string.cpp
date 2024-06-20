52dc15a6ee26bd49ab31fd52a20c390bf9599bdd
JinbiaoZhu <jbzhu1999@gmail.com>
2024-06-20 16:58:47 +0800

    2024.06.20 noon

---
+++ _KAMA_055_right_rotation_string/_KAMA_055_right_rotation_string.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include<string>
+
+using namespace std;
+
+string reverse_string(string s, int left, int right)
+{
+	while (left <= right)
+	{
+		char temp = s[left];
+		s[left] = s[right];
+		s[right] = temp;
+
+		right--;
+		left++;
+	}
+
+	return s;
+}
+
+int main()
+{
+	std::cout << "Hello World!\n";
+
+	string s;
+	int n;
+
+	cout << "n = ";
+	cin >> n;
+	cout << "s = ";
+	cin >> s;
+
+	s = reverse_string(s, 0, s.size() - 1);
+	s = reverse_string(s, 0, n - 1);
+	s = reverse_string(s, n, s.size() - 1);
+
+	cout << "NEW s = " << s << endl;
+
+	return 0;
+}
